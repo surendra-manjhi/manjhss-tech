8606589f05553c73f03727995f66b3a856886fd2
Surendra <manjhisurendra08@gmail.com>
2024-01-02 22:35:07 +0530

    Added new imgs

--- src/data/data.js
+++ src/data/data.js
@@ -103,15 +103,9 @@ export const PROJECTS = [
 
 export const COVER_IMG = [
 	{
-		url: "https://media.istockphoto.com/id/1059211766/photo/dog-face-cute-happy-pet-puppy-looking-in-the-grass.webp?b=1&s=170667a&w=0&k=20&c=gBDF2yyLJiHVc86xDBy5JIYSuYCm8_-6g3t26rZsRis=",
+		url: "https://preview.redd.it/does-anyone-have-a-real-github-contributions-graph-like-v0-useo8hrsi6k91.png?auto=webp&s=923482c46ee77d6ff216cda0204f51cf113671bc",
 	},
 	{
-		url: "https://media.istockphoto.com/id/1189009449/photo/eye-vision-test-with-sight-chart-optometrist-technology.webp?b=1&s=170667a&w=0&k=20&c=jJ2RQxAbTxrw-exwfvrALZHB8OOcXvr3Pe7m4nz1Tbk=",
-	},
-	{
-		url: "https://media.istockphoto.com/id/482745207/photo/message-paper.webp?b=1&s=170667a&w=0&k=20&c=7RoM1YLn2AfaDTAtfweyQ5KbC2UDF3FzxaTqdjposFg=",
-	},
-	{
-		url: "https://media.istockphoto.com/id/672781504/photo/airplane-with-banner.webp?b=1&s=170667a&w=0&k=20&c=3enI5XvygqF61pOhIn4fKulzXDBtAPTNke7j53lvW80=",
+		url: "https://i.imgur.com/piFIRf8.png",
 	},
 ];
