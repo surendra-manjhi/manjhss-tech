37f325dbad420e524bc057fb0bb05ae9591632b8
Surendra <manjhisurendra08@gmail.com>
2024-01-02 22:22:15 +0530

    Make slide workable

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
 
 function Banner() {
-	const covers = [
-		"https://cultivatedculture.com/wp-content/uploads/2020/06/LinkedIn-Banner-Image-Example-of-Someone-Hiking-In-The-Mountains.png",
-		"https://business.linkedin.com/content/dam/me/business/en-us/marketing-solutions/events/2017/images/banners/vertical-trust-services-event-dsk-2x.jpg",
-		"https://cdn.wallpapersafari.com/89/8/PhTjr9.jpg",
+	const slides = [
+		{
+			url: "https://media.istockphoto.com/id/1059211766/photo/dog-face-cute-happy-pet-puppy-looking-in-the-grass.webp?b=1&s=170667a&w=0&k=20&c=gBDF2yyLJiHVc86xDBy5JIYSuYCm8_-6g3t26rZsRis=",
+		},
+		{
+			url: "https://media.istockphoto.com/id/1189009449/photo/eye-vision-test-with-sight-chart-optometrist-technology.webp?b=1&s=170667a&w=0&k=20&c=jJ2RQxAbTxrw-exwfvrALZHB8OOcXvr3Pe7m4nz1Tbk=",
+		},
+		{
+			url: "https://media.istockphoto.com/id/482745207/photo/message-paper.webp?b=1&s=170667a&w=0&k=20&c=7RoM1YLn2AfaDTAtfweyQ5KbC2UDF3FzxaTqdjposFg=",
+		},
+		{
+			url: "https://media.istockphoto.com/id/672781504/photo/airplane-with-banner.webp?b=1&s=170667a&w=0&k=20&c=3enI5XvygqF61pOhIn4fKulzXDBtAPTNke7j53lvW80=",
+		},
 	];
 
-	const [count, setCount] = useState(2);
-	const [next, setNext] = useState(covers[count]);
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-	function handleClick() {}
+	function nextSlide() {
+		const isLastSlide = currentIndex == slides.length - 1;
+		const newIndex = isLastSlide ? 0 : currentIndex + 1;
+		setCurrentIndex(newIndex);
+	}
+
 	return (
 		<div className="relative">
-			<img src={next} alt="cover-img" className="w-full" />
+			<img
+				src={slides[currentIndex].url}
+				alt="cover-img"
+				className="w-full"
+			/>
+
 			<button
-				onClick={handleClick}
+				onClick={nextSlide}
 				className="material-symbols-rounded absolute top-[50%] right-8 text-4xl text-white bg-gray-500/70 hover:bg-gray-500 w-10 h-10 rounded-full"
 			>
 				navigate_next
