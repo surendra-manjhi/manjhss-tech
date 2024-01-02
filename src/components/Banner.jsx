a84794ee6ddaf5e3bd5aa9518881ae1a8c7ff5cf
Surendra <manjhisurendra08@gmail.com>
2024-01-02 22:27:55 +0530

    Made seperate imgs data in data file

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,9 @@
 import React, { useState } from "react";
 
-function Banner() {
-	const slides = [
-		{
-			url: "https://media.istockphoto.com/id/1059211766/photo/dog-face-cute-happy-pet-puppy-looking-in-the-grass.webp?b=1&s=170667a&w=0&k=20&c=gBDF2yyLJiHVc86xDBy5JIYSuYCm8_-6g3t26rZsRis=",
-		},
-		{
-			url: "https://media.istockphoto.com/id/1189009449/photo/eye-vision-test-with-sight-chart-optometrist-technology.webp?b=1&s=170667a&w=0&k=20&c=jJ2RQxAbTxrw-exwfvrALZHB8OOcXvr3Pe7m4nz1Tbk=",
-		},
-		{
-			url: "https://media.istockphoto.com/id/482745207/photo/message-paper.webp?b=1&s=170667a&w=0&k=20&c=7RoM1YLn2AfaDTAtfweyQ5KbC2UDF3FzxaTqdjposFg=",
-		},
-		{
-			url: "https://media.istockphoto.com/id/672781504/photo/airplane-with-banner.webp?b=1&s=170667a&w=0&k=20&c=3enI5XvygqF61pOhIn4fKulzXDBtAPTNke7j53lvW80=",
-		},
-	];
+import { COVER_IMG } from "../data/data";
 
+function Banner() {
+	const slides = [...COVER_IMG];
 	const [currentIndex, setCurrentIndex] = useState(0);
 
 	function nextSlide() {
--- src/data/data.js
+++ src/data/data.js
@@ -100,3 +100,18 @@ export const PROJECTS = [
 		tags: ["react", "APIs"],
 	},
 ];
+
+export const COVER_IMG = [
+	{
+		url: "https://media.istockphoto.com/id/1059211766/photo/dog-face-cute-happy-pet-puppy-looking-in-the-grass.webp?b=1&s=170667a&w=0&k=20&c=gBDF2yyLJiHVc86xDBy5JIYSuYCm8_-6g3t26rZsRis=",
+	},
+	{
+		url: "https://media.istockphoto.com/id/1189009449/photo/eye-vision-test-with-sight-chart-optometrist-technology.webp?b=1&s=170667a&w=0&k=20&c=jJ2RQxAbTxrw-exwfvrALZHB8OOcXvr3Pe7m4nz1Tbk=",
+	},
+	{
+		url: "https://media.istockphoto.com/id/482745207/photo/message-paper.webp?b=1&s=170667a&w=0&k=20&c=7RoM1YLn2AfaDTAtfweyQ5KbC2UDF3FzxaTqdjposFg=",
+	},
+	{
+		url: "https://media.istockphoto.com/id/672781504/photo/airplane-with-banner.webp?b=1&s=170667a&w=0&k=20&c=3enI5XvygqF61pOhIn4fKulzXDBtAPTNke7j53lvW80=",
+	},
+];
